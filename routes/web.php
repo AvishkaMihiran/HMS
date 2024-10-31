c850ba6e97402c83aab7a7c294a72c57fa7d64c1
Kosala-Sakhila <kosalasakhila19@gmail.com>
2024-10-31 21:33:09 +0530

    my delete
    delete

--- routes/web.php
+++ routes/web.php
@@ -41,23 +41,8 @@ Route::get('/food-ordering', function () {
     return view('food-ordering'); 
 });
 
-Route::get('/admin', function () {
-    return view('admin.dashboard'); 
-});
 Route::get('/cook', function () {
     return view('cook'); 
 })->middleware('auth');;
 
-Route::get('/create_room', function () {
-    return view('admin.create_room'); 
-});
-
 Route::get("/redirects",[HomeController::class,"redirects"]);
-
-route::post('/add_room',[AdminController::class,'add_room']);
-
-//Route::get('/view_room',[AdminController::class,'view_room'])->name('admin.view_room');
-
-route::get('/room_delete/{id}',[AdminController::class,'room_delete'])->name('admin.room_delete');;
-
-route::post('/add_room',[AdminController::class,'add_room']);
