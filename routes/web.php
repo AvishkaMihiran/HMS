d86b44424b927a18e0a9e9f2d1f0e599113c3f57
Kosala-Sakhila <kosalasakhila19@gmail.com>
2024-11-01 00:31:22 +0530

    WEB

--- routes/web.php
+++ routes/web.php
@@ -36,5 +36,4 @@ route::get('/room_delete/{id}',[AdminController::class,'room_delete'])->name('ad
 
 Route::get('/update_room/{id}', [AdminController::class, 'update_room'])->name('update_room');
 
-Route::post('/edit_room/{id}', [AdminController::class, 'edit_room']);
 Route::post('/edit_room/{id}', [AdminController::class, 'edit_room'])->name('data.edit_room');
