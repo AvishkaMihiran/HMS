0b7cd47f8c854f3ecac1eb1198b0e890eaad8a38
Kosala-Sakhila <kosalasakhila19@gmail.com>
2024-11-04 16:34:55 +0530

    log out admin

--- resources/views/admin/slidebar.blade.php
+++ resources/views/admin/slidebar.blade.php
@@ -14,7 +14,6 @@
                 <li><a href="{{url('admin')}}"> <i class="icon-home"></i>Home </a></li>
                 <li><a href="{{url('all_msg')}}"> <i class="icon-mail"></i>Send message</a></li>
                 <li><a href="{{url('table')}}"> <i class="icon-grid"></i>Tables </a></li>
-                <li><a href="charts.html"> <i class="fa fa-bar-chart"></i>Charts </a></li>
                 <li><a href="forms.html"> <i class="icon-padnote"></i>Forms </a></li>
                 <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-windows"></i>Room</a>
                   <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
@@ -23,13 +22,6 @@
                     <li><a href="{{url('boking_aprove')}}">Booking Approve</a></li>
                   </ul>
                 </li>
-                <li><a href="login.html"> <i class="icon-logout"></i>Login page </a></li>
-        </ul><span class="heading">Extra</span>
-        <ul class="list-unstyled">
-          <li> <a href="#"> <i class="icon-settings"></i>Demo </a></li>
-          <!--<li> <a href="#"> <i class="icon-writing-whiteboard"></i>Demo </a></li>
-          <li> <a href="#"> <i class="icon-chart"></i>Demo </a></li>-->
-        </ul>
       </nav>
       <!-- Sidebar Navigation end-->
     
